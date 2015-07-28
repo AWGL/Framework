69c4e69efd7cfb53adc439a9df0537d3eedf37e1
msl1y11 <mlyon@live.co.uk>
2015-07-28 10:48:39 +0100

    update

--- MalformedBEDRecordException.java
+++ MalformedBEDRecordException.java
@@ -4,14 +4,10 @@ package nhs.genetics.cardiff;
  * Created by ml on 23/03/15.
  */
 
-public class MalformedBEDRecordException extends RuntimeException {
-
-    public MalformedBEDRecordException(String message) {
+class MalformedBEDRecordException extends Exception
+{
+    public MalformedBEDRecordException(String message)
+    {
         super(message);
     }
-    public String getMessage()
-    {
-        return super.getMessage();
-    }
-
 }
