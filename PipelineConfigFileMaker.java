fc12949aaea9c8d5ca41fd2bebf6c41231e55120
mcgml <mcgml@cardiff.ac.uk>
2016-08-09 16:04:00 +0100

    Updated for new pipeline

--- PipelineConfigFileMaker.java
+++ PipelineConfigFileMaker.java
@@ -1,13 +1,16 @@
 package nhs.genetics.cardiff.framework;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Created by msl on 27/01/2015.
+ * A class for writing config files for pipelines
+ *
+ * @author  Matt Lyon
+ * @version 1.1
+ * @since   2015-01-27
  */
 public class PipelineConfigFileMaker {
 
@@ -17,22 +20,6 @@ public class PipelineConfigFileMaker {
     {
         log.log(Level.INFO, "Writing variable files...");
 
-        StringBuilder analysisDirs = new StringBuilder("AnalysisDirs=( ");
-
-        for (IlluminaSampleSheetRecord record : illuminaSampleSheetFile.getSampleSheetRecords())
-        {
-            analysisDirs.append('"');
-            analysisDirs.append("/data/results");
-            analysisDirs.append("/");
-            analysisDirs.append(illuminaRunParametersFile.getRunIdentifier());
-            analysisDirs.append('/');
-            analysisDirs.append(record.getSampleID());
-            analysisDirs.append('"');
-            analysisDirs.append(' ');
-        }
-
-        analysisDirs.append(')');
-
         for (int n = 0; n < illuminaSampleSheetFile.getSampleSheetRecords().size(); ++n) {
 
             try (PrintWriter writer = new PrintWriter(illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleID() + ".variables")){
@@ -40,42 +27,18 @@ public class PipelineConfigFileMaker {
                 //write variable files
                 writer.print("#Illumina Variables File\n");
 
-                writer.print("\n#Run Details\n");
-                writer.print("RunID=" + illuminaRunParametersFile.getRunIdentifier() + "\n");
-                writer.print("ExperimentName=\"" + illuminaSampleSheetFile.getExperimentName() + "\"\n");
-                writer.print("Platform=ILLUMINA\n");
-
                 writer.print("\n#Demographics\n");
-                writer.print("SampleID=" + illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleID() + "\n");
+                writer.print("sampleId=" + illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleID() + "\n");
 
-                writer.print("\n#FASTQ Filenames\n");
-                writer.print("Read1Fastq=" + illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleID() + "_S" + (n + 1) + "_L001_R1_001.fastq.gz\n");
-                writer.print("Read2Fastq=" + illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleID() + "_S" + (n + 1) + "_L001_R2_001.fastq.gz\n");
-
-                if (!illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleProject().equals("")){
-                    writer.print("\n#Sample Project\n");
-
-                    StringBuilder sampleProjects = new StringBuilder("SampleProjects=( ");
-
-                    for (String field : illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSampleProject().split(";")){
-                        sampleProjects.append('"');
-                        sampleProjects.append(field);
-                        sampleProjects.append('"');
-                        sampleProjects.append(' ');
-                    }
-
-                    sampleProjects.append(')');
-
-                    writer.print(sampleProjects.toString() + "\n");
-                }
+                writer.print("\n#Run Details\n");
+                writer.print("seqId=" + illuminaRunParametersFile.getRunIdentifier() + "\n");
+                writer.print("platform=ILLUMINA\n");
+                writer.print("worklistId=\"" + illuminaSampleSheetFile.getSampleSheetRecords().get(n).getSamplePlate() + "\"\n");
 
                 writer.print("\n#Sample Descriptors\n");
                 for (String field : illuminaSampleSheetFile.getSampleSheetRecords().get(n).getDescription().split(";")){
                     writer.print(field + "\n");
                 }
-
-                writer.print("\n#Analysis Folders\n");
-                writer.print(analysisDirs.toString() + "\n");
 
                 writer.close();
 
@@ -85,4 +48,5 @@ public class PipelineConfigFileMaker {
         }
 
     }
+
 }
